f22fb06ad238e13e833c2d026607f34fa3b0fecb
Rinat Enikeev <rinat.enikeev@gmail.com>
2013-11-20 15:12:04 +0600

    restrictions reviewed

--- restrictions.h
+++ restrictions.h
@@ -1,18 +1,15 @@
 #ifndef RESTRICTIONS_DECL_H
 #define RESTRICTIONS_DECL_H
 
-#define NUM_FUNC		  100
-#define NUM_GLOBAL_VARS   4
-#define NUM_GLOBAL_ARRAYS 4
-#define NUM_LOCAL_VARS    4
-#define NUM_LOCAL_ARRS    4
-#define NUM_BLOCK         100
-#define ID_LEN            31
-#define FUNC_CALLS        31
-#define NUM_PARAMS        31
-#define PROG_SIZE         10000
-#define LOOP_NEST         31
-#define FOR_NEST          31
+#define NUM_FUNC		  100   /* максимальное количество функций */
+#define NUM_GLOBAL_VARS   400   /* максимальное количество глобальных переменных */
+#define NUM_GLOBAL_ARRAYS 400   /* максимальное количество глобальных массивов */
+#define NUM_LOCAL_VARS    400   /* максимальное количество локальных переменных */
+#define NUM_LOCAL_ARRS    400   /* максимальное количество глобальных массивов */
+#define NUM_BLOCK         100   /* максимальная вложенность блоков кода*/
+#define ID_LEN            31    /* максимальная длина идентификатора*/
+#define NUM_PARAMS        31    /* максимальное количество параметров функции */
+#define PROG_SIZE         10000 /* максимальный	размер программы в символах */
 
 #define DEBUG 1
 

b5a4f0c05cf777f41c33d4651fbc6c18de46ad67
Rinat Enikeev <rinat.enikeev@gmail.com>
2013-11-21 15:59:12 +0600

    removed switch

--- commonEnums.h
+++ commonEnums.h
@@ -2,7 +2,6 @@
 #define COMMON_ENUMS_DECL_H
 
 /**********************************************************************/
-/* Типы лексем */
 enum tok_types {DELIMITER,  // знаки пунктуации и операторы
 				IDENTIFIER, // имена переменных и функций
 				NUMBER,     // числовая константа
@@ -14,26 +13,19 @@ enum tok_types {DELIMITER,  // знаки пунктуации и операторы
 				ARRAY};		// [ or ]
 				
 
-/* Зарезервированные слова */
 enum tokens {ARG, 
 			CHAR, 
 			INT, 
-			IF, 
+			IF,
 			ELSE, 
 			FOR, 
 			DO, 
 			WHILE,
-            SWITCH, 
 			RETURN, 
 			EOL, 
 			FINISHED, 
 			END};
 
-/* Эти константы используются для вызова функции sntx_err()
-   в случае синтаксической ошибки. 
-   ВНИМАНИЕ: константа SYNTAX используется тогда, когда
-   интерпритатор не может квалифицировать ошибку.
-*/
 enum error_msg
      {SYNTAX, UNBAL_PARENS, NO_EXP, EQUALS_EXPECTED,
       NOT_VAR, PARAM_ERR, SEMI_EXPECTED,

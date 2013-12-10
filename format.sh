378bf62b2ed9b88f0d2874e6485866f768ed1ec6
Aynur Ishbuldin <iiinjoy@gmail.com>
2013-12-10 16:47:14 +0600

    added script for code formatting (need Artistic Style)

---
+++ format.sh
@@ -0,0 +1,7 @@
+astyle --recursive -A2 -t2 -U -m0 -j -k3 -W3 --mode=c -c -M72 -n "*.cpp"
+astyle --recursive -A2 -t2 -U -m0 -j -k3 -W3 --mode=c -c -M72 -n "*.hpp"
+astyle --recursive -A2 -t2 -U -m0 -j -k3 -W3 --mode=c -c -M72 -n "*.h"
+astyle --recursive -A2 -t2 -U -m0 -j -k3 -W3 --mode=c -c -M72 -n "*.c"
+astyle --recursive -A2 -t2 -U -m0 -j -k3 -W3 --mode=c -c -M72 -n "*.hh"
+astyle --recursive -A2 -t2 -U -m0 -j -k3 -W3 --mode=c -c -M72 -n "*.cc"
+astyle --recursive -A2 -t2 -U -m0 -j -k3 -W3 --mode=c -c -M72 -n "*.test"
